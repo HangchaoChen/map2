7f12cb6ec2c19e80d0063bd7f2f68af1eda94bee
Hangchao Chen <hangchaochen.k@gmail.com>
2019-11-14 22:00:27 -0500

    setting done

--- src/app/setting/setting.component.ts
+++ src/app/setting/setting.component.ts
@@ -8,6 +8,29 @@ import { Component, OnInit } from "@angular/core";
 export class SettingComponent implements OnInit {
   selectedYear;
 
+  numberOfDistrict;
+
+  weight1 = 1;
+
+  weight2 = 2;
+
+  weight3 = 3;
+
+  updateBehavior = false;
+
+  onClickSwitch() {
+    console.log("update switch");
+    this.updateBehavior = !this.updateBehavior;
+  }
+  onClickApply() {
+    console.log("year: ", this.selectedYear);
+    console.log("numberOfDistrict: ", this.numberOfDistrict);
+    console.log("w1: ", this.weight1);
+    console.log("w2: ", this.weight2);
+    console.log("w3: ", this.weight3);
+    console.log("update: ", this.updateBehavior);
+  }
+
   constructor() {}
 
   ngOnInit() {}
