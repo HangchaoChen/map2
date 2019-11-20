533c97d4927740135d4196baa69db8a060085f5a
Hangchao Chen <hangchaochen.k@gmail.com>
2019-11-19 21:27:44 -0500

    sider socket removed

--- src/app/sider/sider.component.ts
+++ src/app/sider/sider.component.ts
@@ -1,40 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 
-import * as Stomp from "stompjs";
-import * as SockJS from "sockjs-client";
-import $ from "jquery";
-
 @Component({
   selector: "app-sider",
   templateUrl: "./sider.component.html",
   styleUrls: ["./sider.component.css"]
 })
 export class SiderComponent implements OnInit {
-  constructor() {
-    this.initializeWebSocketConnection();
-  }
-  private serverUrl = "http://localhost:8080/socket";
-  private stompClient;
-
-  initializeWebSocketConnection() {
-    let ws = new SockJS(this.serverUrl);
-    this.stompClient = Stomp.over(ws);
-    let that = this;
-    this.stompClient.connect({}, function(frame) {
-      that.stompClient.subscribe("/chat", message => {
-        console.log("subscribed: ");
-        if (message.body) {
-          //$(".chat").append("<div class='message'>" + message.body + "</div>");
-          console.log("message.body :", message.body);
-        }
-      });
-    });
-  }
-
-  sendMessage(message) {
-    this.stompClient.send("/app/send/message", {}, message);
-    $("#input").val("");
-  }
+  constructor() {}
 
   openSetting = false;
   showSettings() {
@@ -51,7 +23,6 @@ export class SiderComponent implements OnInit {
 
   onClickApply() {
     console.log("apply clicked");
-    this.sendMessage("test");
     console.log("send messgae call");
   }
 
